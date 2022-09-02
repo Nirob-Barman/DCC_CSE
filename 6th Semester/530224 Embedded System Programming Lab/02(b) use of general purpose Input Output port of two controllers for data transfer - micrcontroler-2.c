6bc945292a574d2b04ca09719a50c7e341023870
NiRob BarMan <47166768+rsnirob@users.noreply.github.com>
2022-09-02 15:05:40 +0600

    Update 02(b) use of general purpose Input Output port of two controllers for data transfer - micrcontroler-2.c

--- 6th Semester/530224 Embedded System Programming Lab/02(b) use of general purpose Input Output port of two controllers for data transfer - micrcontroler-2.c
+++ 6th Semester/530224 Embedded System Programming Lab/02(b) use of general purpose Input Output port of two controllers for data transfer - micrcontroler-2.c
@@ -5,7 +5,7 @@ void Delay(unsigned int time);
 void main(void)
 {
 	while(1)
-    {
+	{
 		P2 = P1;
 		Delay(1000);
 	}
@@ -15,7 +15,7 @@ void Delay(unsigned int time)
 {
 	unsigned int i,j;
 	for (i = 0; i < time; i++)
-    {
+    	{
 		for (j = 0; j < 23; j++)
 		{
 		
